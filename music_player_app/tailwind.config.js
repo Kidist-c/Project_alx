74941be7887547f24ae1a4e64b11fd3233fdad94
kidistMulualem <mulualemkidist4@gmail.com>
2025-03-25 14:42:41 +0900

    started builiding didebar

--- music_player_app/tailwind.config.js
+++ music_player_app/tailwind.config.js
@@ -2,7 +2,9 @@ module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // You can set it to 'media' or 'class' if needed
   theme: {
-    extend: {},
+    extend: {
+      
+    },
   },
   plugins: [],
 };
